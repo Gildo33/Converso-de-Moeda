60614c45deecf525367027946b71b844cd476097
Gildo33 <gildofelismino38@gmailcom>
2024-08-21 19:06:57 -0300

    imagens

---
+++ 2.Script.js
@@ -0,0 +1,36 @@
+console.log("Olá,agora estou por aqui")
+
+/*
+
+Document - HTML
+
+getElementById -> Trás um elemento pelo ID
+getElementByClassName -> Trás todos elementos com essa classe.
+getElementByTagName -> Trás todos elementos com essa TAG.
+getElementByName -> Trás todos elementos com esse name.
+
+querySelector -> Trás o elemento,o primeiro que encontrar.
+querySelectorAll -> Trás todos os elementos que encontrar.
+
+
+*/ 
+
+//const elements = document.getElementsByClassName("paragraph-js")
+
+//console.log(elements)
+
+//const input = document.getElementById("main-input")
+//console.log (input)
+
+//const tag = document.getElementsByTagName("h1")
+//console.log (tag)
+
+//const name = document.getElementsByName("nome-completo")
+//console.log (name)
+
+const selector = document.querySelector ("img")
+
+
+console.log (selector.src)
+
+
---
+++ 3.0Byid.js
@@ -0,0 +1,60 @@
+/*const input = document.querySelector(".main-button")
+
+input.innerHTML = "A linguagem de <b>programação</b> mais versatil do mundo"
+
+console.log(input.textContent) //Só html
+console.log(input.innerText) //Leva em conta o css
+console.log(input.innerHTML) //trás tudo -> permite adicionar tegs html*/
+
+/*const button = document.querySelector("button") //Alterando CSS(estilos) no javascript
+
+button.style.color = "red"*/
+
+
+//Eventos
+
+
+
+
+
+
+let input =document.querySelector("#main-input")
+
+function cliqueiNoBotao () {
+    console.log(input.value)
+
+      
+}
+
+function digiteiNoInput(){
+    console.log("digitei no input")
+
+    const element = document.querySelector("input.value") 
+
+    
+}
+
+const elementos =document.querySelector(".main-button")
+function digiteiNoInput() {
+    console.log(element.innerHTML)
+    
+}
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
